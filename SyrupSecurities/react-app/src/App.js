27592c94195fbce7a27c0d71a3ed8559ff019845
IglooSyrup <darckuvina@gmail.com>
2022-02-25 14:38:35 -0500

    React - Removed Styles and Added Routes to the new components

--- SyrupSecurities/react-app/src/App.js
+++ SyrupSecurities/react-app/src/App.js
@@ -3,14 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Home from './Components/Homepage/Home';
 import Navbar from './Components/Navbar/Navbar';
+import Error404 from './Components/Error404/Error404';
 
 function App() {
+
+  // TODO Cache the user credentials
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact render={() => (
           <>
             <Home />
+          </>
+        )}/>
+        <Route path="/home" exact render={() => (
+          <>
+            <Home />
+          </>
+        )}/>
+        {/* <Route path="/antivirus" exact render={() => (
+          <>
+            <Error404 />
+          </>
+        )}/>
+        <Route path="/vpn" exact render={() => (
+          <>
+            <Error404 />
+          </>
+        )}/>
+        <Route path="/products" exact render={() => (
+          <>
+            <Error404 />
+          </>
+        )}/> */}
+        <Route path="/*" render={() => (
+          <>
+            <Error404 />
           </>
         )}/>
       </Switch>
