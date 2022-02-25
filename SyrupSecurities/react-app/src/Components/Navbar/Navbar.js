a1022f21a7249977c391e29e7a7de1708c2d4647
IglooSyrup <darckuvina@gmail.com>
2022-02-25 14:41:53 -0500

    React - Added Navbar and a few routes (Still in development)

--- SyrupSecurities/react-app/src/Components/Navbar/Navbar.js
+++ SyrupSecurities/react-app/src/Components/Navbar/Navbar.js
@@ -1,39 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from "react-router-dom"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
 
 const Navbar = () => {
+    const [customer, setCustomer] = useState({
+        matricule: ""
+    })
+    const history = useHistory()
+    // const historyState = history.location.state
+    // const customerState = historyState.student
+    const location = useLocation()
+
+    const toHome =() =>{
+        history.push("/home")
+    }
+
+    const toAntivirus = () =>{
+        history.push("/antivirus")
+    }
+
+    const toVpn = () =>{
+        history.push("/vpn")
+    }
+
+    const toProducts = () =>{
+        history.push("/products")
+    }
+
+    const toTeam = () =>{
+        history.push("/team")
+    }
+
+    const toContactUs = () =>{
+        history.push("/contact")
+    }
+
     return (
-        <nav className="navbar navbar-light navbar-expand">
-                <a className="nav-link" href="#hero">Home</a>
-                <a className="nav-link " href="#about">About Us</a>
-                <a className="nav-link " href="#services">Services</a>
-                <a className="nav-link " href="#portfolio">Portfolio</a>
-                <a className="nav-link " href="#team">Team</a>
-                {/* <li className="dropdown"><a href="#"><span>Drop Down</span> <i className=""></i></a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className=""></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
-                </li> */}
-                <a className="nav-link " href="#contact">Contact</a>
-            <i className=""></i>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-brand btn btn-link">
+                <h4 className="text-dark align-middle">
+                    Syrup Securities
+                </h4>
+            </button>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item ">
+                        <button className="nav-link btn btn-link text-primary" onClick={()=> toHome()}>Accueil </button>
+                        {/* <span class="sr-only">(current)</span> */}
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link btn btn-link text-primary" onClick={()=> toAntivirus()}>Antivirus</button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link btn btn-link text-primary" onClick={()=> toVpn()}>V.P.N</button>
+                    </li>
+                    <li className="nav-item ">
+                        <button className="nav-link btn btn-link text-primary"  onClick={()=>toProducts()}>Tous les Produits</button>
+                    </li>
+                    <li className="nav-item ">
+                        <button className="nav-link btn btn-link text-primary" onClick={()=> toTeam()}>L'équipe</button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link btn btn-link text-primary" onClick={()=> toContactUs()}>Nous joindre</button>
+                    </li>
+                </ul>
+                {/* TODO  login/registration landing page*/}
+                <span className="navbar-text">
+                    Se connecter/S'inscrire
+                </span>
+            </div>
         </nav>
-        // <nav className="navbar navbar-light bg-light">
-        //     <a className="navbar-brand" href="#">
-        //         <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt=""/>
-        //             Bootstrap
-        //     </a>
-        // </nav>
     )
 }
 
